374a36cca6ece6f03a94de4f3b7984baa5d95b64
Rishabh Lakhaiyar <rishabhlakhaiyar27@gmail.com>
2022-07-03 19:13:31 +0530

    files chamged

--- test1.java
+++ test1.java
@@ -1,20 +1,7 @@
-import java.util.Scanner;
+public class test1{
+    public static void main(String[] args){
+        System.out.println("hello world");
 
-class first {
-
-    public static void main(String[] args) {
-        System.out.println("enter no");
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=a+10;
-        System.out.println(b);
-
-
-       
-	
     }
+ 
 }
-
-
-
-
