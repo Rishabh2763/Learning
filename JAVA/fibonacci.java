8e411a041573243bfc14c1426a8baba3df164c85
Rishabh Lakhaiyar <rishabhlakhaiyar27@gmail.com>
2022-07-06 01:30:13 +0530

    files added,changed,deleted

---
+++ JAVA/fibonacci.java
@@ -0,0 +1,24 @@
+public class fibonacci{
+
+    // 0,1,1,2,3,5,8...
+
+    static int logic(int a , int b){
+        int z=a+b;
+        return z;
+
+    }
+    public static void main(String[] args){
+
+        int num1=0;
+        int num2=1;
+        int sum=logic(num1, num2);
+        System.out.println(sum);
+
+
+
+    }
+
+
+
+ 
+}
---
+++ JAVA/loops.java
@@ -0,0 +1,17 @@
+public class loops{
+
+    
+    public static void main(String[] args) {
+        int num=1;
+
+
+        do {
+            System.out.println(num);
+            num++;
+        } while (num<10);
+        
+    
+        
+    }
+    
+}
